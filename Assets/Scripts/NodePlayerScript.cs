f10cabfb8172ce0cd59c2030aace9a952f35ce70
NicholasDewberryOfficial <85315604+NicholasDewberryOfficial@users.noreply.github.com>
2021-09-10 13:31:35 -0400

    a

---
+++ Assets/Scripts/NodePlayerScript.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NodePlayerScript : MonoBehaviour
+{
+    public int Health;
+    public int corruptionLevel;
+    public bool hasChainsaw;
+    public bool hasPlasmaGun;
+    public bool hasAssaultRifle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Health = 10;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
